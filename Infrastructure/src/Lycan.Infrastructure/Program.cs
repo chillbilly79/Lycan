dbe5f49c8b7dc3091df2c6cd92d18a81283b1fac
chillbilly79 <brnco79@gmail.com>
2021-02-16 11:41:36 -0800

    Casey/cdk (#28)
    * restructure folders
    
    * cdk infrastructure project

---
+++ Infrastructure/src/Lycan.Infrastructure/Program.cs
@@ -0,0 +1,18 @@
+﻿using Amazon.CDK;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Lycan.Infrastructure;
+
+namespace Infrastructure
+{
+    sealed class Program
+    {
+        public static void Main(string[] args)
+        {
+            var app = new App();
+            var stack = new LycanStack(app, "LycanStack");
+            app.Synth();
+        }
+    }
+}
