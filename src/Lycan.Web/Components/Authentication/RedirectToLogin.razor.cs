8a679fb7f5d08bddfb200cdb057ec876ff45fff0
zhlavinka <zhlavinka@gmail.com>
2021-02-21 12:15:47 -0800

    fix authentication/login route redirect

--- src/Lycan.Web/Components/Authentication/RedirectToLogin.razor.cs
+++ src/Lycan.Web/Components/Authentication/RedirectToLogin.razor.cs
@@ -6,13 +6,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Lycan.Web.Components.Auth {
+namespace Lycan.Web.Components.Authentication {
   public partial class RedirectToLogin : ComponentBase {
     [Inject]
     NavigationManager Navigation { get; set; }
 
     protected override void OnInitialized() {
-      Navigation.NavigateTo($"authentication/login/returnUrl={Uri.EscapeDataString(Navigation.Uri)}");
+      Navigation.NavigateTo($"authentication/login?returnUrl={Uri.EscapeDataString(Navigation.Uri)}");
     }
   }
 }
